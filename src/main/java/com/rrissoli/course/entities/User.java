c99dd3b3f1aa184c6392b83a325ff4d5c0ca8151
Rrissoli <raphaelcrissoli@gmail.com>
2023-03-10 20:49:23 -0300

    implements H2 as database for tests

--- src/main/java/com/rrissoli/course/entities/User.java
+++ src/main/java/com/rrissoli/course/entities/User.java
@@ -3,8 +3,19 @@ package com.rrissoli.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name="tb_user")
 public class User implements Serializable{
-	private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private Long id;
 		private String Name;
 		private String email;

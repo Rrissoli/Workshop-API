2017264b8f6403d7491572091c657abdb7a57549
Rrissoli <raphaelcrissoli@gmail.com>
2023-03-13 16:36:41 -0300

    Service layer and user service  was add

--- src/main/java/com/rrissoli/course/config/TestConfig.java
+++ src/main/java/com/rrissoli/course/config/TestConfig.java
@@ -18,7 +18,6 @@ public class TestConfig implements  CommandLineRunner  {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		userRepository.saveAll(Arrays.asList(u1,u2));

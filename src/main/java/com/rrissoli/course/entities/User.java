8b98d5383b8b3a8aa42c5daea68461ed2263c92d
Rrissoli <raphaelcrissoli@gmail.com>
2023-03-13 17:11:23 -0300

    Service Order add

--- src/main/java/com/rrissoli/course/entities/User.java
+++ src/main/java/com/rrissoli/course/entities/User.java
@@ -1,86 +1,105 @@
 package com.rrissoli.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
-
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 @Entity
-@Table(name="tb_user")
-public class User implements Serializable{
-		private static final long serialVersionUID = 1L;
+@Table(name = "tb_user")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String Name;
+	private String email;
+	private String phone;
+	private String password;
+	@OneToMany(mappedBy = "client" )
+	private List<Order> orders = new ArrayList<>();
+
 	
-		@Id
-		@GeneratedValue(strategy = GenerationType.IDENTITY)
-		private Long id;
-		private String Name;
-		private String email;
-		private String phone;
-		private String password;
-		
-		public String getName() {
-			return Name;
-		}
-		public void setName(String name) {
-			Name = name;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		public String getPhone() {
-			return phone;
-		}
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
-		
-		public Long geId() {
-			return id;
-		}
-		public void setId(Long id) {
-			this.id = id;
-		}
-		
-		public User() {
-			
-		}
-		public User(Long id, String name, String email, String phone, String password) {
-			super();
-			this.id = id;
-			Name = name;
-			this.email = email;
-			this.phone = phone;
-			this.password = password;
-		}
-		
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			User other = (User) obj;
-			return Objects.equals(Name, other.Name) && Objects.equals(email, other.email)
-					&& Objects.equals(id, other.id) && Objects.equals(password, other.password)
-					&& Objects.equals(phone, other.phone);
-		}
-		@Override
-		public int hashCode() {
-			return Objects.hash(Name, email, id, password, phone);
-		}
+	public List<Order> getOrder() {
+		return orders;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long geId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User() {
+
+	}
+
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		Name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(Name, other.Name) && Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(password, other.password) && Objects.equals(phone, other.phone);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Name, email, id, password, phone);
+	}
 }

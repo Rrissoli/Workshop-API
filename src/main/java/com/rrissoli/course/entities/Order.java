b5e225b630425776c9dbe6a7114e157bd854fa65
Rrissoli <raphaelcrissoli@gmail.com>
2023-03-14 19:37:15 -0300

    implented orderStatus enum

--- src/main/java/com/rrissoli/course/entities/Order.java
+++ src/main/java/com/rrissoli/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rrissoli.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,12 +26,24 @@ public class Order implements Serializable {
 	private Long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
+	
 	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "client_Id")
 	private User client;
+	
+	private Integer orderStatus;
 
 	public Order() {
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+		this.orderStatus = orderStatus.getCode();}
 	}
 
 	public Long getId() {
@@ -57,11 +70,12 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
 	@Override

a944cc0d5cfb95e5039e6ce0d2db56f4e29fe1fe
Rrissoli <raphaelcrissoli@gmail.com>
2023-03-16 10:07:17 -0300

    suibtotal e total

--- src/main/java/com/rrissoli/course/entities/OrderItem.java
+++ src/main/java/com/rrissoli/course/entities/OrderItem.java
@@ -60,7 +60,9 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id, price, quantity);

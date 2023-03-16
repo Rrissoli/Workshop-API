26eb2de6b63d41eedf95a77183a6fe046a9ce18c
Rrissoli <raphaelcrissoli@gmail.com>
2023-03-16 10:53:17 -0300

    user delete and insert

--- src/main/java/com/rrissoli/course/resources/UserResource.java
+++ src/main/java/com/rrissoli/course/resources/UserResource.java
@@ -1,31 +1,54 @@
 package com.rrissoli.course.resources;
 
+import java.net.URI;
 import java.util.List;
+
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rrissoli.course.entities.User;
 import com.rrissoli.course.services.UserService;
 
+import jakarta.servlet.Servlet;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
-		@Autowired
-		private  UserService service;
-		@GetMapping
-		public ResponseEntity<List<User>> findAll() {
-			List<User> list = service.findAll();
-			return ResponseEntity.ok().body(list);
-		}
-		@GetMapping(value =  "/{id}")
-		public ResponseEntity<User> findById(@PathVariable Long  id){
-			User obj = service.findById(id);
-			return ResponseEntity.ok().body(obj);
-		}
+
+	@Autowired
+	private UserService service;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.geId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.Delete(id);
+		return ResponseEntity.noContent().build();
+	}
 }
